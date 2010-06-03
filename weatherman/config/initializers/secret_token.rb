46334d2bd150fdf09faf75171bce49f7172db2e0
Paul-Michael Agapow <github@agapow.net>
2010-06-03 09:41:33 +0100

    initial commit

---
+++ weatherman/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '973ff74e97c22ccdbd581d3a73410a87fefd628408edf09bbe9b334505ef7c2f89ab02ecdc4d45f98572dbe268596982e62236e07bec000c4d79dfe7818422db'
